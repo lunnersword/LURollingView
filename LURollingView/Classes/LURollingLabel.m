ee4d1de18d65ce41ecddd25a8700a047ecf1e084
lunnersword <lunnersword@163.com>
2016-11-28 15:06:51 +0800

    add ,

--- LURollingView/Classes/LURollingLabel.m
+++ LURollingView/Classes/LURollingLabel.m
@@ -59,7 +59,7 @@
 - (void)layoutSubviews {
     [super layoutSubviews];
     self.scrollView.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
-}
+};
 
 - (void)setFrame:(CGRect)frame {
     CGRect bounds = self.bounds;
